e98dd5347b5af17c1fc61afcdd49aa2d1d360e5e
Francesc Martinez <francescmm@gmail.com>
2019-10-23 20:52:33 +0200

    Fixing dependencies for Linux

--- ci-scripts/linux/install.sh
+++ ci-scripts/linux/install.sh
@@ -1,7 +1,2 @@
-sudo add-apt-repository --yes ppa:beineri/opt-qt597-trusty
-sudo add-apt-repository --yes ppa:achadwick/mypaint-testing
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get -qq install g++-4.9 libc6-i386
-export CXX="g++-4.9 -std=c++14"
-export CC="gcc-4.9"
-
+sudo apt-get -qq install g++-7 libc6-i386 build-essential libgl1-mesa-dev mesa-common-dev libgles2-mesa-dev
