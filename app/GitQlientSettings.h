9c1a24f9071461897bd498f0de700480ea928f54
Francesc M <francescmm@gmail.com>
2019-11-10 14:11:43 +0100

    Fixing license text

--- app/GitQlientSettings.h
+++ app/GitQlientSettings.h
@@ -8,7 +8,7 @@
  ** LinkedIn: www.linkedin.com/in/cescmm/
  ** Web: www.francescmm.com
  **
- ** This library is free software; you can redistribute it and/or
+ ** This program is free software; you can redistribute it and/or
  ** modify it under the terms of the GNU Lesser General Public
  ** License as published by the Free Software Foundation; either
  ** version 2 of the License, or (at your option) any later version.
