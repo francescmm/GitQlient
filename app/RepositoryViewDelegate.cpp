0d5217aabda5cf5ddae5e4ae1d93de6cbd0ac018
Francesc Martinez <francescmm@gmail.com>
2019-11-15 18:56:25 +0100

    Cleaning some code

--- app/RepositoryViewDelegate.cpp
+++ app/RepositoryViewDelegate.cpp
@@ -31,29 +31,19 @@ static QColor blend(const QColor &col1, const QColor &col2, int amount = 128)
 void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, int x2, const QColor &col,
                                             const QColor &activeCol, const QBrush &back, bool isWip) const
 {
-   const int padding = 2;
+   const auto padding = 2;
    x1 += padding;
    x2 += padding;
 
-   int h = ROW_HEIGHT / 2;
-   int m = (x1 + x2) / 2;
-   int r = (x2 - x1) * 1 / 3;
-   int d = 2 * r;
-
-#define P_CENTER m, h
-#define P_0 x2, h // >
-#define P_90 m, 0 // ^
-#define P_180 x1, h // <
-#define P_270 m, 2 * h // v
-#define DELTA_UR 2 * (x1 - m), 2 * h, 0 * 16, 90 * 16 // -,
-#define DELTA_DR 2 * (x1 - m), 2 * -h, 270 * 16, 90 * 16 // -'
-#define DELTA_UL 2 * (x2 - m), 2 * h, 90 * 16, 90 * 16 //  ,-
-#define DELTA_DL 2 * (x2 - m), 2 * -h, 180 * 16, 90 * 16 //  '-
-#define CENTER_UR x1, 2 * h, 225
-#define CENTER_DR x1, 0, 135
-#define CENTER_UL x2, 2 * h, 315
-#define CENTER_DL x2, 0, 45
-#define R_CENTER m - r, h - r, d, d
+   const auto h = ROW_HEIGHT / 2;
+   const auto m = (x1 + x2) / 2;
+   const auto r = (x2 - x1) * 1 / 3;
+   const auto d = 2 * r;
+   const auto spanAngle = 90 * 16;
+   const auto angleWidthRight = 2 * (x1 - m);
+   const auto angleWidthLeft = 2 * (x2 - m);
+   const auto angleHeightUp = 2 * h;
+   const auto angleHeightDown = 2 * -h;
 
    static QColor const &lanePenColor = QPalette().color(QPalette::WindowText);
    static QPen lanePen(lanePenColor, 2); // fast path here
@@ -65,31 +55,31 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::JOIN_R:
       case LaneType::HEAD:
       case LaneType::HEAD_R: {
-         QConicalGradient gradient(CENTER_UR);
+         QConicalGradient gradient(x1, 2 * h, 225);
          gradient.setColorAt(0.375, col);
          gradient.setColorAt(0.625, activeCol);
          lanePen.setBrush(gradient);
          p->setPen(lanePen);
-         p->drawArc(P_CENTER, DELTA_UR);
+         p->drawArc(m, h, angleWidthRight, angleHeightUp, 0 * 16, spanAngle);
          break;
       }
       case LaneType::JOIN_L: {
-         QConicalGradient gradient(CENTER_UL);
+         QConicalGradient gradient(2, 2 * h, 315);
          gradient.setColorAt(0.375, activeCol);
          gradient.setColorAt(0.625, col);
          lanePen.setBrush(gradient);
          p->setPen(lanePen);
-         p->drawArc(P_CENTER, DELTA_UL);
+         p->drawArc(m, h, angleWidthLeft, angleHeightUp, 90 * 16, spanAngle);
          break;
       }
       case LaneType::TAIL:
       case LaneType::TAIL_R: {
-         QConicalGradient gradient(CENTER_DR);
+         QConicalGradient gradient(x1, 0, 135);
          gradient.setColorAt(0.375, activeCol);
          gradient.setColorAt(0.625, col);
          lanePen.setBrush(gradient);
          p->setPen(lanePen);
-         p->drawArc(P_CENTER, DELTA_DR);
+         p->drawArc(m, h, angleWidthRight, angleHeightDown, 270 * 16, spanAngle);
          break;
       }
       default:
@@ -111,11 +101,11 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::JOIN_R:
       case LaneType::JOIN_L:
       case LaneType::CROSS:
-         p->drawLine(P_90, P_270);
+         p->drawLine(m, 0, m, 2 * h);
          break;
       case LaneType::HEAD_L:
       case LaneType::BRANCH:
-         p->drawLine(P_CENTER, P_270);
+         p->drawLine(m, h, m, 2 * h);
          break;
       case LaneType::TAIL_L:
       case LaneType::INITIAL:
@@ -123,7 +113,7 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::BOUNDARY_C:
       case LaneType::BOUNDARY_R:
       case LaneType::BOUNDARY_L:
-         p->drawLine(P_90, P_CENTER);
+         p->drawLine(m, 0, m, h);
          break;
       default:
          break;
@@ -142,17 +132,17 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::CROSS:
       case LaneType::CROSS_EMPTY:
       case LaneType::BOUNDARY_C:
-         p->drawLine(P_180, P_0);
+         p->drawLine(x1, h, x2, h);
          break;
       case LaneType::MERGE_FORK_R:
       case LaneType::BOUNDARY_R:
-         p->drawLine(P_180, P_CENTER);
+         p->drawLine(x1, h, m, h);
          break;
       case LaneType::MERGE_FORK_L:
       case LaneType::HEAD_L:
       case LaneType::TAIL_L:
       case LaneType::BOUNDARY_L:
-         p->drawLine(P_CENTER, P_0);
+         p->drawLine(m, h, x2, h);
          break;
       default:
          break;
@@ -166,14 +156,14 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::BRANCH:
          p->setPen(Qt::black);
          p->setBrush(col);
-         p->drawEllipse(R_CENTER);
+         p->drawEllipse(m - r, h - r, d, d);
          break;
       case LaneType::MERGE_FORK:
       case LaneType::MERGE_FORK_R:
       case LaneType::MERGE_FORK_L:
          p->setPen(Qt::black);
          p->setBrush(col);
-         p->drawRect(R_CENTER);
+         p->drawRect(m - r, h - r, d, d);
          break;
       case LaneType::UNAPPLIED:
          // Red minus sign
@@ -191,32 +181,18 @@ void RepositoryViewDelegate::paintGraphLane(QPainter *p, LaneType type, int x1, 
       case LaneType::BOUNDARY:
          p->setPen(Qt::black);
          p->setBrush(back);
-         p->drawEllipse(R_CENTER);
+         p->drawEllipse(m - r, h - r, d, d);
          break;
       case LaneType::BOUNDARY_C:
       case LaneType::BOUNDARY_R:
       case LaneType::BOUNDARY_L:
          p->setPen(Qt::black);
          p->setBrush(back);
-         p->drawRect(R_CENTER);
+         p->drawRect(m - r, h - r, d, d);
          break;
       default:
          break;
    }
-#undef P_CENTER
-#undef P_0
-#undef P_90
-#undef P_180
-#undef P_270
-#undef DELTA_UR
-#undef DELTA_DR
-#undef DELTA_UL
-#undef DELTA_DL
-#undef CENTER_UR
-#undef CENTER_DR
-#undef CENTER_UL
-#undef CENTER_DL
-#undef R_CENTER
 }
 
 void RepositoryViewDelegate::paint(QPainter *p, const QStyleOptionViewItem &opt, const QModelIndex &index) const
@@ -268,13 +244,7 @@ void RepositoryViewDelegate::paintGraph(QPainter *p, const QStyleOptionViewItem 
 
    if (r.sha().isEmpty())
       return;
-   /*
-      if (r.isDiffCache && !mGit->isNothingToCommit() && !mView->hasActiveFiler())
-      {
-         paintWip(p, opt);
-         return;
-      }
-   */
+
    p->save();
    p->setClipRect(opt.rect, Qt::IntersectClip);
    p->translate(opt.rect.topLeft());
@@ -293,11 +263,14 @@ void RepositoryViewDelegate::paintGraph(QPainter *p, const QStyleOptionViewItem 
       }
    }
 
-   int x1 = 0, x2 = 0;
-   int maxWidth = opt.rect.width();
-   QColor activeColor = colors[activeLane % COLORS_NUM];
+   auto x1 = 0;
+   auto x2 = 0;
+   const auto maxWidth = opt.rect.width();
+   auto activeColor = colors[activeLane % COLORS_NUM];
+
    if (opt.state & QStyle::State_Selected)
       activeColor = blend(activeColor, opt.palette.highlightedText().color(), 208);
+
    for (auto i = 0; i < laneNum && x2 < maxWidth; i++)
    {
 
@@ -337,8 +310,7 @@ void RepositoryViewDelegate::paintWip(QPainter *painter, QStyleOptionViewItem op
    opt.font.setBold(true);
 
    const auto name = QString("WIP");
-   QFontMetrics fm(opt.font);
-   const auto textBoundingRect = fm.boundingRect(name);
+   const auto textBoundingRect = QFontMetrics(opt.font).boundingRect(name);
    const auto fontRect = textBoundingRect.height();
 
    painter->save();
@@ -355,8 +327,7 @@ void RepositoryViewDelegate::paintWip(QPainter *painter, QStyleOptionViewItem op
 
 void RepositoryViewDelegate::paintLog(QPainter *p, const QStyleOptionViewItem &opt, const QModelIndex &index) const
 {
-   int row = index.row();
-   const auto sha = mGit->getCommitInfoByRow(row).sha();
+   const auto sha = mGit->getCommitInfoByRow(index.row()).sha();
 
    if (sha.isEmpty())
       return;
@@ -379,9 +350,6 @@ void RepositoryViewDelegate::paintLog(QPainter *p, const QStyleOptionViewItem &o
 void RepositoryViewDelegate::paintTagBranch(QPainter *painter, QStyleOptionViewItem o, int &startPoint,
                                             const QString &sha) const
 {
-   const auto showMinimal = o.rect.width() <= MIN_VIEW_WIDTH_PX;
-   const int mark_spacing = 5; // Space between markers in pixels
-
    QMap<QString, QColor> markValues;
    auto ref_types = mGit->checkRef(sha);
    const auto currentBranch = mGit->getCurrentBranchName();
@@ -408,7 +376,10 @@ void RepositoryViewDelegate::paintTagBranch(QPainter *painter, QStyleOptionViewI
          markValues.insert(ref, QColor("#FF5555"));
    }
 
+   const auto showMinimal = o.rect.width() <= MIN_VIEW_WIDTH_PX;
+   const int mark_spacing = 5; // Space between markers in pixels
    const auto mapEnd = markValues.constEnd();
+
    for (auto mapIt = markValues.constBegin(); mapIt != mapEnd; ++mapIt)
    {
       const auto isCurrentSpot = (mapIt.key() == "detached" && currentBranch.isEmpty()) || mapIt.key() == currentBranch;
