726d533bd277e1961b5c74bb175845457b323fad
Francesc M <francescmm@gmail.com>
2020-01-13 21:12:30 +0100

    #42 Fixing typo

--- git/GitBranches.cpp
+++ git/GitBranches.cpp
@@ -35,63 +35,63 @@ GitExecResult GitBranches::getDistanceBetweenBranches(bool toMaster, const QStri
 
 GitExecResult GitBranches::createBranchFromAnotherBranch(const QString &oldName, const QString &newName)
 {
-   QLog_Debug("Git", QString("Executiong createBranchFromAnotherBranch: {%1} and {%2}").arg(oldName, newName));
+   QLog_Debug("Git", QString("Executing createBranchFromAnotherBranch: {%1} and {%2}").arg(oldName, newName));
 
    return mGitBase->run(QString("git branch %1 %2").arg(newName, oldName));
 }
 
 GitExecResult GitBranches::createBranchAtCommit(const QString &commitSha, const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong createBranchAtCommit: {%1} at {%2}").arg(branchName, commitSha));
+   QLog_Debug("Git", QString("Executing createBranchAtCommit: {%1} at {%2}").arg(branchName, commitSha));
 
    return mGitBase->run(QString("git branch %1 %2").arg(branchName, commitSha));
 }
 
 GitExecResult GitBranches::checkoutRemoteBranch(const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong checkoutRemoteBranch: {%1}").arg(branchName));
+   QLog_Debug("Git", QString("Executing checkoutRemoteBranch: {%1}").arg(branchName));
 
    return mGitBase->run(QString("git checkout -q %1").arg(branchName));
 }
 
 GitExecResult GitBranches::checkoutNewLocalBranch(const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong checkoutNewLocalBranch: {%1}").arg(branchName));
+   QLog_Debug("Git", QString("Executing checkoutNewLocalBranch: {%1}").arg(branchName));
 
    return mGitBase->run(QString("git checkout -b %1").arg(branchName));
 }
 
 GitExecResult GitBranches::renameBranch(const QString &oldName, const QString &newName)
 {
-   QLog_Debug("Git", QString("Executiong renameBranch: {%1} at {%2}").arg(oldName, newName));
+   QLog_Debug("Git", QString("Executing renameBranch: {%1} at {%2}").arg(oldName, newName));
 
    return mGitBase->run(QString("git branch -m %1 %2").arg(oldName, newName));
 }
 
 GitExecResult GitBranches::removeLocalBranch(const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong removeLocalBranch: {%1}").arg(branchName));
+   QLog_Debug("Git", QString("Executing removeLocalBranch: {%1}").arg(branchName));
 
    return mGitBase->run(QString("git branch -D %1").arg(branchName));
 }
 
 GitExecResult GitBranches::removeRemoteBranch(const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong removeRemoteBranch: {%1}").arg(branchName));
+   QLog_Debug("Git", QString("Executing removeRemoteBranch: {%1}").arg(branchName));
 
    return mGitBase->run(QString("git push --delete origin %1").arg(branchName));
 }
 
 GitExecResult GitBranches::getBranchesOfCommit(const QString &sha)
 {
-   QLog_Debug("Git", QString("Executiong removeBranchesOfCommit: {%1}").arg(sha));
+   QLog_Debug("Git", QString("Executing removeBranchesOfCommit: {%1}").arg(sha));
 
    return mGitBase->run(QString("git branch --contains %1 --all").arg(sha));
 }
 
 GitExecResult GitBranches::getLastCommitOfBranch(const QString &branch)
 {
-   QLog_Debug("Git", QString("Executiong getLastCommitOfBranch: {%1}").arg(branch));
+   QLog_Debug("Git", QString("Executing getLastCommitOfBranch: {%1}").arg(branch));
 
    auto ret = mGitBase->run(QString("git rev-parse %1").arg(branch));
 
@@ -103,7 +103,7 @@ GitExecResult GitBranches::getLastCommitOfBranch(const QString &branch)
 
 GitExecResult GitBranches::pushUpstream(const QString &branchName)
 {
-   QLog_Debug("Git", QString("Executiong pushUpstream: {%1}").arg(branchName));
+   QLog_Debug("Git", QString("Executing pushUpstream: {%1}").arg(branchName));
 
    return mGitBase->run(QString("git push --set-upstream origin %1").arg(branchName));
 }
--- git/GitHistory.cpp
+++ git/GitHistory.cpp
@@ -12,14 +12,14 @@ GitHistory::GitHistory(const QSharedPointer<GitBase> &gitBase)
 
 GitExecResult GitHistory::blame(const QString &file, const QString &commitFrom)
 {
-   QLog_Debug("Git", QString("Executiong blame: {%1} from {%2}").arg(file, commitFrom));
+   QLog_Debug("Git", QString("Executing blame: {%1} from {%2}").arg(file, commitFrom));
 
    return mGitBase->run(QString("git annotate %1 %2").arg(file, commitFrom));
 }
 
 GitExecResult GitHistory::history(const QString &file)
 {
-   QLog_Debug("Git", QString("Executiong history: {%1}").arg(file));
+   QLog_Debug("Git", QString("Executing history: {%1}").arg(file));
 
    return mGitBase->run(QString("git log --follow --pretty=%H %1").arg(file));
 }
@@ -28,7 +28,7 @@ GitExecResult GitHistory::getCommitDiff(const QString &sha, const QString &diffT
 {
    if (!sha.isEmpty())
    {
-      QLog_Debug("Git", QString("Executiong getCommitDiff: {%1} to {%2}").arg(sha, diffToSha));
+      QLog_Debug("Git", QString("Executing getCommitDiff: {%1} to {%2}").arg(sha, diffToSha));
 
       QString runCmd = QString("git diff-tree --no-color -r --patch-with-stat -m");
 
@@ -47,14 +47,14 @@ GitExecResult GitHistory::getCommitDiff(const QString &sha, const QString &diffT
       return mGitBase->run(runCmd);
    }
    else
-      QLog_Warning("Git", QString("Executiong getCommitDiff with empty SHA"));
+      QLog_Warning("Git", QString("Executing getCommitDiff with empty SHA"));
 
    return qMakePair(false, QString());
 }
 
 QString GitHistory::getFileDiff(const QString &currentSha, const QString &previousSha, const QString &file)
 {
-   QLog_Debug("Git", QString("Executiong getFileDiff: {%1} between {%2} and {%3}").arg(file, currentSha, previousSha));
+   QLog_Debug("Git", QString("Executing getFileDiff: {%1} between {%2} and {%3}").arg(file, currentSha, previousSha));
 
    const auto ret = mGitBase->run(QString("git diff -U15000 %1 %2 %3").arg(previousSha, currentSha, file));
 
@@ -66,7 +66,7 @@ QString GitHistory::getFileDiff(const QString &currentSha, const QString &previo
 
 GitExecResult GitHistory::getDiffFiles(const QString &sha, const QString &diffToSha)
 {
-   QLog_Debug("Git", QString("Executiong getDiffFiles: {%1} to {%2}").arg(sha, diffToSha));
+   QLog_Debug("Git", QString("Executing getDiffFiles: {%1} to {%2}").arg(sha, diffToSha));
 
    QString runCmd = QString("git diff-tree -C --no-color -r -m ");
 
