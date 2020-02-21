e6c9fc490d0c53835a4624dfcf4cff333bec7433
Francesc M <francescmm@gmail.com>
2020-02-21 18:53:34 +0100

    Adding libgl1-mesa-dev

--- ci-scripts/linux/install.sh
+++ ci-scripts/linux/install.sh
@@ -1,3 +1,4 @@
+sudo apt-get -qq install libgl1-mesa-dev
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
 sudo apt-get -qq install g++-7 libc6-i386 build-essential libgl1-mesa-dev mesa-common-dev libgles2-mesa-dev
