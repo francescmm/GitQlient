155bc61ebd527472862abcd5dbf5db00594a65ea
Francesc M <francescmm@gmail.com>
2020-03-09 08:03:35 +0100

    #50 Adding doxygen for some diff widgets

--- src/diff/CommitDiffWidget.h
+++ src/diff/CommitDiffWidget.h
@@ -7,18 +7,54 @@ class QLabel;
 class FileListWidget;
 class RevisionsCache;
 
+/*!
+ \brief The CommitDiffWidget creates the layout that contains the information of a commit diff. This widget is located
+ in the bottom left corner of the Diff view. It is composed by a list with the files that are part of the commit and two
+ labels that indicates the current SHA and the SHA that is compared with.
+
+To configure it the user just needs to call the \ref configure method with the two SHA of the commits they want to
+compare between.
+
+ \class CommitDiffWidget CommitDiffWidget.h "CommitDiffWidget.h"
+*/
 class CommitDiffWidget : public QFrame
 {
    Q_OBJECT
 
 signals:
+   /*!
+    \brief Signal triggered when the user double clicks a file in the list. This notifies the general Diff view that the
+    user wants to open a file.
+
+    \param currentSha The current commit SHA.
+    \param previousSha The parent SHA of the current commit.
+    \param file The file to show the diff.
+   */
    void signalOpenFileCommit(const QString &currentSha, const QString &previousSha, const QString &file);
+   /*!
+    \brief Signal triggered when the user whats to see the blame history for a given file.
+
+    \param fileName The file name to blame.
+   */
    void signalShowFileHistory(const QString &fileName);
 
 public:
+   /*!
+    \brief Default constructor.
+
+    \param git The git object to execute Git commands.
+    \param cache The internal cache for the current repository.
+    \param parent The parent widget if needed.
+   */
    explicit CommitDiffWidget(QSharedPointer<GitBase> git, QSharedPointer<RevisionsCache> cache,
                              QWidget *parent = nullptr);
 
+   /*!
+    \brief Configures the widget to open a diff between two commits.
+
+    \param firstSha The base commit.
+    \param secondSha The commit we want to compare to.
+   */
    void configure(const QString &firstSha, const QString &secondSha);
 
 private:
--- src/diff/DiffButton.h
+++ src/diff/DiffButton.h
@@ -4,19 +4,51 @@
 
 class QPushButton;
 
+/*!
+ \brief The DiffButton is a special implementation of QFrame that has a similar behaviour to a QPushButton. The main
+ difference is that DiffButton allows the user to emit a close signal that also closes the button. So it's self
+ destructive.
+
+Since it's a speialization for the Diff widget, the DiffButton contains in first place an icon that indicates if the
+button refers to a commit or a single file. Then it shows the file name (or just a label indicating that is a commit
+diff) followed by the two SHAs that are part of the diff. At the end of the button there is a close button that destroys
+the widget and triggers a signal to close the diff view.
+
+ \class DiffButton DiffButton.h "DiffButton.h"
+*/
 class DiffButton : public QFrame
 {
    Q_OBJECT
 
 signals:
+   /*!
+    \brief Signal triggered when the user clicks the DiffButton.
+   */
    void clicked();
 
 public:
+   /*!
+    \brief Default constructor.
+
+    \param text The text that will be shown in the button.
+    \param icon The icon that represents the diff: file or commit.
+    \param parent The parent widget if needed.
+   */
    explicit DiffButton(const QString &text, const QString &icon, QWidget *parent = nullptr);
+   /*!
+    \brief Sets the button as it is selected.
+   */
    void setSelected();
+   /*!
+    \brief Unsets the select status of the button.
+   */
    void setUnselected();
 
 protected:
+   /*!
+    \brief Overrided function to process the press event. Used to trigger the click signal and change the style of the
+    button. \param e The event
+   */
    void mousePressEvent(QMouseEvent *e) override;
 
 private:
