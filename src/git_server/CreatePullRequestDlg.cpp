0c460930dc35ef719bff4889e1e1333312c66b2b
Francesc Martinez <francescmm@gmail.com>
2020-07-12 08:42:35 +0200

    Adding PR dialog functionality

--- src/git_server/CreatePullRequestDlg.cpp
+++ src/git_server/CreatePullRequestDlg.cpp
@@ -1,14 +1,69 @@
 #include "CreatePullRequestDlg.h"
 #include "ui_CreatePullRequestDlg.h"
 
-CreatePullRequestDlg::CreatePullRequestDlg(QWidget *parent) :
-   QDialog(parent),
-   ui(new Ui::CreatePullRequestDlg)
+#include <GitHubRestApi.h>
+#include <GitQlientSettings.h>
+#include <GitConfig.h>
+#include <ServerPullRequest.h>
+
+#include <QMessageBox>
+
+CreatePullRequestDlg::CreatePullRequestDlg(QWidget *parent)
+   : QDialog(parent)
+   , ui(new Ui::CreatePullRequestDlg)
 {
+   QScopedPointer<GitConfig> gitConfig(new GitConfig(mGit));
+   const auto gameServerUrl = gitConfig->getServerUrl();
+
+   if (gameServerUrl.contains("github"))
+   {
+      GitQlientSettings settings;
+      mUserName = settings.value(QString("%1/user").arg(gameServerUrl)).toString();
+      const auto userToken = settings.value(QString("%1/token").arg(gameServerUrl)).toString();
+      const auto repoInfo = gitConfig->getCurrentRepoAndOwner();
+
+      mApi = new GitHubRestApi(repoInfo.first, repoInfo.second, { mUserName, userToken });
+      connect(mApi, &GitHubRestApi::signalIssueCreated, this, &CreatePullRequestDlg::onPullRequestCreated);
+
+      mApi->getMilestones();
+      mApi->requestLabels();
+   }
+
    ui->setupUi(this);
+
+   connect(ui->pbCreate, &QPushButton::clicked, this, &CreatePullRequestDlg::accept);
+   connect(ui->pbClose, &QPushButton::clicked, this, &CreatePullRequestDlg::reject);
 }
 
 CreatePullRequestDlg::~CreatePullRequestDlg()
 {
    delete ui;
+}
+
+void CreatePullRequestDlg::accept()
+{
+   if (ui->leTitle->text().isEmpty() || ui->teDescription->toPlainText().isEmpty())
+      QMessageBox::warning(this, tr("Empty fields"), tr("Please, complete all fields with valid data."));
+   else if (ui->cbOrigin->currentText() == ui->cbDestination->currentText())
+   {
+      QMessageBox::warning(
+          this, tr("Error in the branch selection"),
+          tr("The base branch and the branch to merge from cannot be the same. Please, select different branches."));
+   }
+
+   mApi->createPullRequest({ ui->leTitle->text(), ui->teDescription->toPlainText().toUtf8(),
+                             ui->cbDestination->currentText(), ui->cbOrigin->currentText(), true,
+                             ui->chModify->isChecked(), ui->chDraft->isChecked() });
+}
+
+void CreatePullRequestDlg::onPullRequestCreated(const QString &url)
+{
+   auto finalUrl = url;
+   finalUrl = finalUrl.remove("api.").remove("repos/");
+
+   QMessageBox::information(
+       this, tr("Pull Request created"),
+       tr("The Pull Request has been created. You can <a href=\"%1\">find it here</a>.").arg(finalUrl));
+
+   QDialog::accept();
 }
