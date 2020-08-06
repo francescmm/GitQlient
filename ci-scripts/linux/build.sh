6edebbb619947ff1a7f380d338fb9e473317075f
Francesc M <francesc.martinez@king.com>
2020-08-06 08:14:10 +0200

    Updating scripts download URL

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,4 +1,4 @@
-wget -q -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
+wget -q -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt5.zip;
 unzip -qq qt5.zip;
 export QTDIR=$PWD/qt5
 export PATH=$QTDIR/bin:$PATH;
