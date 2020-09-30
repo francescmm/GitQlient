3a0e903235ca68d8001a6e9ba6967c95ccbebd18
Francesc Martinez <francescmm@gmail.com>
2020-09-30 23:26:56 +0200

    #157 Missing correct path for qmake

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -4,7 +4,7 @@ export QTDIR=$PWD/qt5
 export PATH=$QTDIR/bin:$PATH;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 mkdir build
-$QTDIR/bin/qmake ../GitQlient.pro PREFIX=$(pwd)/AppImage/gitqlient/usr
+$QTDIR/bin/qmake GitQlient.pro PREFIX=$(pwd)/AppImage/gitqlient/usr
 make -j 4
 make install
 wget -q -O linuxdeployqt https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
