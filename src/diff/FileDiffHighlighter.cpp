1233216f9933147d2721144e2976fa5b93e5dcc4
Francesc Martinez <francescmm@gmail.com>
2020-10-04 08:46:20 +0200

    Fixing empty lines not shown as diff

--- src/diff/FileDiffHighlighter.cpp
+++ src/diff/FileDiffHighlighter.cpp
@@ -12,51 +12,48 @@ void FileDiffHighlighter::highlightBlock(const QString &text)
 {
    setCurrentBlockState(previousBlockState() + 1);
 
-   if (!text.isEmpty())
+   QTextBlockFormat myFormat;
+   QTextCharFormat format;
+   const auto currentLine = currentBlock().blockNumber() + 1;
+
+   if (!mFileDiffInfo.isEmpty())
    {
-      QTextBlockFormat myFormat;
-      QTextCharFormat format;
-      const auto currentLine = currentBlock().blockNumber() + 1;
-
-      if (!mFileDiffInfo.isEmpty())
+      for (const auto &diff : qAsConst(mFileDiffInfo))
       {
-         for (const auto &diff : qAsConst(mFileDiffInfo))
+         if (diff.startLine <= currentLine && currentLine <= diff.endLine)
          {
-            if (diff.startLine <= currentLine && currentLine <= diff.endLine)
+            if (diff.addition)
             {
-               if (diff.addition)
-               {
-                  myFormat.setBackground(GitQlientStyles::getGreen());
-                  // myFormat.setForeground(GitQlientStyles::getGreen());
-               }
-               else
-                  myFormat.setBackground(GitQlientStyles::getRed());
+               myFormat.setBackground(GitQlientStyles::getGreen());
+               // myFormat.setForeground(GitQlientStyles::getGreen());
             }
+            else
+               myFormat.setBackground(GitQlientStyles::getRed());
          }
       }
-      else
+   }
+   else if (!text.isEmpty())
+   {
+      switch (text.at(0).toLatin1())
       {
-         switch (text.at(0).toLatin1())
-         {
-            case '@':
-               myFormat.setForeground(GitQlientStyles::getOrange());
-               format.setFontWeight(QFont::ExtraBold);
-               break;
-            case '+':
-               myFormat.setForeground(GitQlientStyles::getGreen());
-               break;
-            case '-':
-               myFormat.setForeground(GitQlientStyles::getRed());
-               break;
-            default:
-               break;
-         }
+         case '@':
+            myFormat.setBackground(GitQlientStyles::getOrange());
+            format.setFontWeight(QFont::ExtraBold);
+            break;
+         case '+':
+            myFormat.setBackground(GitQlientStyles::getGreen());
+            break;
+         case '-':
+            myFormat.setBackground(GitQlientStyles::getRed());
+            break;
+         default:
+            break;
       }
+   }
 
-      if (myFormat.isValid())
-      {
-         QTextCursor(currentBlock()).setBlockFormat(myFormat);
-         setFormat(0, currentBlock().length(), format);
-      }
+   if (myFormat.isValid())
+   {
+      QTextCursor(currentBlock()).setBlockFormat(myFormat);
+      setFormat(0, currentBlock().length(), format);
    }
 }
