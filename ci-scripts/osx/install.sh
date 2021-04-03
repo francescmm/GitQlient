6a36238d2a82ba3e3df61993cc5264099bde9d99
Francesc Martinez <francescmm@gmail.com>
2021-04-03 16:47:37 +0200

    Changes in MaxOS CI

--- ci-scripts/osx/install.sh
+++ ci-scripts/osx/install.sh
@@ -1,4 +1,5 @@
-brew install glew
-brew install clang-format
-brew install qt
-brew install create-dmg
+HOMEBREW_NO_AUTO_UPDATE=1 brew install glew
+HOMEBREW_NO_AUTO_UPDATE=1 brew install clang-format
+HOMEBREW_NO_AUTO_UPDATE=1 brew install qt
+HOMEBREW_NO_AUTO_UPDATE=1 brew install create-dmg
+HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --force java
