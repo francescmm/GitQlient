eb862fdf373a4eb33d99510e34e96dd56ef0d6a2
Francesc Martinez <francescmm@gmail.com>
2021-04-25 17:39:43 +0200

    Script update for new version os MacOS

--- ci-scripts/osx/install.sh
+++ ci-scripts/osx/install.sh
@@ -2,4 +2,4 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install glew
 HOMEBREW_NO_AUTO_UPDATE=1 brew install clang-format
 HOMEBREW_NO_AUTO_UPDATE=1 brew install qt
 HOMEBREW_NO_AUTO_UPDATE=1 brew install create-dmg
-HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --force java
+HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --force --ignore-dependencies java
